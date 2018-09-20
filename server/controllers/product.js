ff9ca60ce0e01190b3c66fdf287963e056567758
jolowres <jolowres@gmail.com>
2018-09-20 08:12:28 +0100

    returns a list of all products

--- server/controllers/product.js
+++ server/controllers/product.js
@@ -1,4 +1,35 @@
 const fs = require('fs')
+const DATA_DIR = './data'
+
+function readFiles() {
+
+}
+
+function getProducts (req, res, next) {
+    var products = []
+
+
+    var myPromise = new Promise((resolve, reject) => {
+        fs.readdir(DATA_DIR, (err, filenames) =>  {
+            resolve(filenames)
+        })
+    })
+
+    myPromise.then(filenames => Promise.all(filenames.map((filename) => {
+        return new Promise((resolve, reject) => {
+            var fullPath = DATA_DIR + '/' + filename
+            fs.readFile(fullPath, (err, data) => {
+                if (err) throw err
+                products.push(JSON.parse(data))
+                resolve()
+            })
+        })
+    }))
+
+    .then(() => {
+        res.send(products)
+    }))
+}
 
 function getProduct (req, res, next) {
     var products
@@ -15,5 +46,6 @@ function updateProduct (req, res, next) {
 
 module.exports = {
     getProduct,
+    getProducts,
     updateProduct
 }
--- server/services/product.js
+++ server/services/product.js
@@ -1,7 +1,7 @@
 const productController = require('../controllers/product')
 const router = require('express').Router()
 
-router.get('/product', productController.getProduct)
+router.get('/product', productController.getProducts)
 router.post('/product', productController.updateProduct)
 
 module.exports = router
